0c5b9a4ddb0660028e1b2b50e1a5d7355a128f4b
Frehitzz <fritzharlydegamo@gmail.com>
2026-07-12 11:13:55 +0800

    test: add relationship tests for Message and User models

---
+++ tests/Feature/MessageTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('can get the sender and receiver of a message',
+    function () {
+        // 1. ARRANGE: created 2 fake users in our testing db
+        $userA = User::factory()->create();
+        $userB = User::factory()->create();
+
+        // 2. ACT: created message sent from user A to user B
+        $message = Message::create([
+            'sender_id' => $userA->id,
+            'receiver_id' => $userB->id,
+            'body' => 'HELLO THIS IS FROM USER A',
+            'is_read' => false,
+        ]);
+
+        // 3. ASSERT: check if the relationship actually work
+
+        // does the message's sender match user A?
+        expect($message->sender->id)->toBe($userA->id);
+
+        // does the message's receiver match user B?
+        expect($message->receiver->id)->toBe($userB->id);
+
+        // does User A sent messages contiane this message?
+        expect($userA->sentMessage->contains($message))->toBeTrue();
+
+    });
